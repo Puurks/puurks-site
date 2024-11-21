3217e0f8a8d82a5dafef55b76a6657dd67952785
Puurks <96667467+Puurks@users.noreply.github.com>
2024-11-20 20:27:39 -0600

    Create parallax.js

---
+++ parallax.js
@@ -0,0 +1,4 @@
+// initiate parallax effects
+var selectscene = document.getElementById('select-screen');
+var parallaxInstance = new Parallax(selectscene);
+// you can change parallax options using parallaxInstance
